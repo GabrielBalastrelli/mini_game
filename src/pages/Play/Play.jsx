6f18240e71c06c382e14df8ff0492eacf8a9136a
= <=>
2025-05-14 21:19:55 -0300

    subindo o jogo

--- src/pages/Play/Play.jsx
+++ src/pages/Play/Play.jsx
@@ -1,11 +1,18 @@
 import styles from "./Play.module.css";
 
 const Play = () => {
-    return (
-        <div className={styles.container_main}>
-            <h1>Shortly!</h1>
-        </div>
-    );
+  return (
+    <div className={styles.container_main}>
+      <h1>Aprecie nosso mini game!</h1>
+      <iframe
+        src="game/index.html"
+        width="800"
+        height="600"
+        frameborder="0"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default Play;
